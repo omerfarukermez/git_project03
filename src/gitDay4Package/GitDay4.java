58e177eff4d2e5a8eb3f9ef14cd03e467f6dcdbc
OmerErmez <omerfe92@gmail.com>
2020-03-17 00:51:15 +0100

    Local Branch First

--- src/gitDay4Package/GitDay4.java
+++ src/gitDay4Package/GitDay4.java
@@ -1,5 +1,10 @@
 package gitDay4Package;
 
 public class GitDay4 {
+	
+	public static void main(String[] args) {
+		System.out.println("local first commit master");
+		System.out.println("First Branch");
+	}
 
 }
